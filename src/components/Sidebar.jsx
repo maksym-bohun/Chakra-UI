a3daf1d5470f28b54f4952e990039b4e43e628d0
Maksym-Bohun <maks.boggun@gmail.com>
2023-10-03 19:09:24 +0300

    used Lists and List Icons

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import { AtSignIcon, CalendarIcon, EditIcon } from "@chakra-ui/icons";
+import { List, ListIcon, ListItem, Text } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <List color="white" fontSize="1.2rem" spacing={5}>
+      <ListItem>
+        <NavLink to="/">
+          {" "}
+          <ListIcon as={CalendarIcon} /> Dashboard
+        </NavLink>
+      </ListItem>
+
+      <ListItem>
+        <NavLink to="/create">
+          <ListIcon as={EditIcon} /> New Task
+        </NavLink>
+      </ListItem>
+
+      <ListItem>
+        <NavLink to="/profile">
+          {" "}
+          <ListIcon as={AtSignIcon} />
+          Profile
+        </NavLink>
+      </ListItem>
+    </List>
+  );
+};
+
+export default Sidebar;
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { Grid, GridItem } from "@chakra-ui/react";
+import Sidebar from "../components/Sidebar";
 
 export default function RootLayout() {
   return (
@@ -9,10 +10,10 @@ export default function RootLayout() {
         as="aside"
         colSpan={{ base: 6, lg: 2, xl: 1 }}
         bg="purple.400"
-        minHeight="100vh"
+        minHeight={{ lg: "100vh" }}
         p={{ base: "20px", lg: "30px" }}
       >
-        <span>sidebar</span>
+        <Sidebar />
       </GridItem>
 
       <GridItem as="main" colSpan={{ base: 6, lg: 4, xl: 5 }}>
