42cfd1f5a5ab4218d410b067bf2e32c9450017d7
Maksym-Bohun <maks.boggun@gmail.com>
2023-10-05 18:23:44 +0300

    used Form Fields

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,42 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import { Form } from "react-router-dom";
+
 export default function Create() {
   return (
-    <div>Create</div>
-  )
+    <Box maxW="480px" ml="30px">
+      <Form>
+        <FormControl isRequired mb="40px">
+          <FormLabel>Task name</FormLabel>
+          <Input type="text" name="title" />
+          <FormHelperText>Enter a descriptive task name.</FormHelperText>
+        </FormControl>
+
+        <FormControl mb="40px">
+          <FormLabel>Task description:</FormLabel>
+          <Textarea
+            placeholder="Enter a detailed decription for the task..."
+            name="description"
+          />
+        </FormControl>
+
+        <FormControl display="flex" alignItems="center" mb="40px">
+          <Checkbox colorScheme="purple" name="isPriority" size="lg" />
+          <FormLabel mb={0} ml="10px">
+            Make this a priority task.
+          </FormLabel>
+        </FormControl>
+
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Box>
+  );
 }
